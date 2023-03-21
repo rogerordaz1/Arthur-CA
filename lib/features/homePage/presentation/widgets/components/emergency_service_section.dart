6a0ae4e4b6395eb0d1d29f85c4697f3b93ba9b31
Roger Ordaz <rogerordaz98@gmail.com>
2023-03-21 13:37:53 -0700

    sigo arreglado el responsive

--- lib/features/homePage/presentation/widgets/components/emergency_service_section.dart
+++ lib/features/homePage/presentation/widgets/components/emergency_service_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class EmergencyServiceSection extends StatelessWidget {
   const EmergencyServiceSection({
@@ -9,14 +10,49 @@ class EmergencyServiceSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 115, left: 180),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      padding: EdgeInsets.only(
+        right: ResponsiveValue(
+          context,
+          defaultValue: 115.0,
+          valueWhen: [
+            const Condition.smallerThan(
+              name: TABLET,
+              value: 80.0,
+            ),
+          ],
+        ).value!,
+        left: ResponsiveValue(
+          context,
+          defaultValue: 115.0,
+          valueWhen: [
+            const Condition.smallerThan(
+              name: TABLET,
+              value: 80.0,
+            ),
+          ],
+        ).value!,
+      ),
+      child: ResponsiveRowColumn(
+        layout: ResponsiveWrapper.of(context).isSmallerThan(TABLET)
+            ? ResponsiveRowColumnType.COLUMN
+            : ResponsiveRowColumnType.ROW,
+        rowCrossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            flex: 3,
+          ResponsiveRowColumnItem(
+            columnOrder: 2,
+            rowFlex: 3,
             child: Padding(
-              padding: const EdgeInsets.only(top: 135),
+              padding: EdgeInsets.only(
+                  top: ResponsiveValue(
+                context,
+                defaultValue: 135.0,
+                valueWhen: [
+                  const Condition.smallerThan(
+                    name: TABLET,
+                    value: 0.0,
+                  ),
+                ],
+              ).value!),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -31,7 +67,17 @@ class EmergencyServiceSection extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 200),
+                  SizedBox(
+                      height: ResponsiveValue(
+                    context,
+                    defaultValue: 200.0,
+                    valueWhen: [
+                      const Condition.smallerThan(
+                        name: TABLET,
+                        value: 100.0,
+                      ),
+                    ],
+                  ).value!),
                   const EmergencyServiceRowItem(
                     textLeft:
                         'Our company has a 24\nemergency service for any\nunexpected situation that\nmay arise for any of our\nclients.',
@@ -43,10 +89,21 @@ class EmergencyServiceSection extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            flex: 2,
+          ResponsiveRowColumnItem(
+            columnOrder: 1,
+            rowFlex: 2,
             child: Container(
-              padding: const EdgeInsets.only(top: 100),
+              padding: EdgeInsets.only(
+                  top: ResponsiveValue(
+                context,
+                defaultValue: 100.0,
+                valueWhen: [
+                  const Condition.smallerThan(
+                    name: TABLET,
+                    value: 0.0,
+                  ),
+                ],
+              ).value!),
               height: 751,
               child: Image.asset(
                 'assets/truck.png',
@@ -74,6 +131,7 @@ class EmergencyServiceRowItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
           child: Row(
@@ -129,7 +187,9 @@ class EmergencyServiceRowItem extends StatelessWidget {
             ],
           ),
         ),
-        const SizedBox(width: 50),
+        const ResponsiveVisibility(hiddenWhen: [
+          Condition.smallerThan(name: TABLET),
+        ], child: SizedBox(width: 50)),
       ],
     );
   }
